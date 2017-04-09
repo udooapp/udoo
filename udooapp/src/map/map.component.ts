dd8b012e5331aed3fe3e72b5e25fb137c0b9966d
zoltanantal <zoltan.antal@rivosoftware.com>
2017-04-09 18:03:21 +0300

    Adding map, header and menu container ui elements

---
+++ udooapp/src/map/map.component.ts
@@ -0,0 +1,54 @@
+import {Component, OnInit} from '@angular/core';
+
+declare var google: any;
+
+@Component({
+  selector: 'map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+
+  private scriptLoadingPromise: Promise<void>;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    this.load().then(() => {
+      const map = new google.maps.Map(document.getElementById('map'), {
+             center: {lat: 48.211029, lng: 16.373990},
+             zoom: 14
+           });
+    });
+  }
+
+  load(): Promise<void> {
+    if (this.scriptLoadingPromise) {
+      return this.scriptLoadingPromise;
+    }
+
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.defer = true;
+
+    const callbackName: string = 'initMap';
+    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCvn27CPRnDIm_ROE-Q8U-x2pUYep7yCmU&callback=' + callbackName;
+
+    this.scriptLoadingPromise = new Promise<void>((resolve: Function, reject: Function) => {
+      (<any>window)[callbackName] = () => {
+        resolve();
+      };
+
+      script.onerror = (error: Event) => {
+        reject(error);
+      };
+    });
+
+    document.body.appendChild(script);
+
+    return this.scriptLoadingPromise;
+  }
+}
